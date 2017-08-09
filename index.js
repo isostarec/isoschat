ff549cd03312bebbfd834570e613373542ae9198
isostarec <sostarecivan@outlook.com>
2017-08-09 17:58:09 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+var users = [];
+
+app.use(express.static('public'));
+server.listen(port);
+console.log('Express listening on port ' + port + '!');
+
+io.on('connection', function(socket){
+
+  socket.on('submituser', function(data){
+    socket.username = data;
+    if(users.indexOf(data) == -1 && data.length) {
+      users.push(data);
+      socket.emit('newuser');
+      console.log('New user logged in with Id of ' + socket.id + ' and username of ' + socket.username + '!');
+    } else if(users.indexOf(data) != -1) {
+      socket.emit('userexists', socket.username);
+    } else if(!data) {
+      socket.emit('invalidusername');
+    }
+
+    socket.on('newmsg', function(msg){
+      if(!msg){
+        socket.emit('invalidmessage');
+      } else {
+        io.sockets.emit('insertmsg', msg, socket.username);  
+      }
+    })
+
+    socket.on('disconnect', function(){
+      console.log('User ' + socket.username + ' with socket ' + socket.id + ' disconnected');
+    })
+
+  })
+
+});
---
+++ public/js/mysocket.js
@@ -0,0 +1,34 @@
+const socket = io.connect('http://localhost:3000');
+
+$('#newuser').submit(function(e){
+  e.preventDefault();
+  socket.emit('submituser', $('#username').val());
+})
+
+socket.on('userexists', function(data) {
+  alert('User ' + data + ' tried to join the chat but already participates!');
+})
+
+socket.on('newuser', function() {
+  $('.login').hide();
+  $('.chat').fadeIn(1500);
+})
+
+socket.on('invalidusername', function(){
+  alert('Username not valid. Please try again.');
+})
+
+socket.on('invalidmessage', function(){
+  alert('Message cannot be empty!');
+})
+
+socket.on('insertmsg', function(msg, username){
+  $('#chatbody').append('<div class="card"><div class="card-block">'+ username + ': ' + msg +'</div></div><br>');
+})
+
+$('#sendmsg').submit(function(e){
+  var msg = $('#msgbox').val();
+  e.preventDefault();
+  socket.emit('newmsg', msg);
+  $('#msgbox').val('');
+});
